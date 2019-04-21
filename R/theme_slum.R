d545a034fc530d941c822ef9b1e161a46b45c662
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-21 13:30:36 +1000

    theme_slume now reads the palette file

--- R/theme_slum.R
+++ R/theme_slum.R
@@ -12,34 +12,21 @@
 #' @export
 theme_slum <- function(palette = "dark", ...) {
 
-  if(palette == "dark") {
-    primary <- "#DDDDDD"
-    subtle <- "#888888"
-    behind <- "#333333"
-  }
+  # check that this is a slumdown project and
+  # retrieve the palette
+  check_slumdown()
+  pal <- slum_getpalette(palette)
 
-  if(palette == "light") {
-    primary <- "#111111"
-    subtle <- "#CCCCCC"
-    behind <- "#FCFCFC"
-  }
-
-  if(palette == "kunoichi") {
-    primary <- "#FFFFFF"
-    subtle <- "#562457"
-    behind <- "#BA68C8"
-  }
-
-  theme_grey(...) %+replace%
+  ggplot2::theme_grey(...) %+replace%
     ggplot2::theme(
-      plot.background = ggplot2::element_rect(fill = behind, colour = behind),
-      panel.background = ggplot2::element_rect(fill = subtle, colour = subtle),
-      legend.background = ggplot2::element_rect(fill = behind, colour = behind),
-      plot.title = ggplot2::element_text(colour = primary),
-      plot.subtitle = ggplot2::element_text(colour = primary),
-      axis.title = ggplot2::element_text(colour = primary),
-      axis.text = ggplot2::element_text(colour = primary),
-      legend.title = ggplot2::element_text(colour = primary),
-      legend.text = ggplot2::element_text(colour = primary)
+      plot.background = ggplot2::element_rect(fill = pal["pagecolour"], colour = pal["pagecolour"]),
+      panel.background = ggplot2::element_rect(fill = pal["faded"], colour = pal["faded"]),
+      legend.background = ggplot2::element_rect(fill = pal["pagecolour"], colour = pal["pagecolour"]),
+      plot.title = ggplot2::element_text(colour = pal["maintext"]),
+      plot.subtitle = ggplot2::element_text(colour = pal["maintext"]),
+      axis.title = ggplot2::element_text(colour = pal["maintext"]),
+      axis.text = ggplot2::element_text(colour = pal["maintext"]),
+      legend.title = ggplot2::element_text(colour = pal["maintext"]),
+      legend.text = ggplot2::element_text(colour = pal["maintext"])
     )
 }
