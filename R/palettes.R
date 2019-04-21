d6fa81a9b42da5ed4f6dbc0f55aa975894df97d5
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-21 14:47:38 +1000

    addpalette function adde

--- R/palettes.R
+++ R/palettes.R
@@ -68,3 +68,48 @@ slum_getpalette <- function(palette) {
   return(values)
 }
 
+
+
+#' Retrieve palette information
+#' @param name Character string with the name of the new palette (e.g., "cerulean")
+#' @param pagecolour Character string with hex code (e.g., "#333333") for the page colour
+#' @param maintext Character string specifying the main text colour
+#' @param faded Character string specifying the "faded" colour
+#' @param highlight Character string for highlight (mostly in links)
+#' @param lowlight Character string for lowlight (mostly in code)
+#' @param overwrite Should an existing file be overwritten?
+#' @details Writes a file to the user directory
+#' @importFrom here here
+#' @export
+slum_addpalette <- function(name, pagecolour, maintext, faded, highlight, lowlight, overwrite = FALSE) {
+
+  check_slumdown()
+
+  # content of the palette file
+  pal <- c(
+    paste0(" /* ", name, " colour scheme */"),
+    ":root { ",
+    paste0("  --pagecolour: ", pagecolour , ";  "),
+    paste0("    --maintext: ", maintext, ";  "),
+    paste0("    --faded: ", faded, "; "),
+    paste0("    --highlight: ", highlight, "; "),
+    paste0("    --lowlight: ", lowlight, "; "),
+    "}"
+  )
+
+  # create a user css directory if needed
+  path <- here::here("static", "css")
+  if(!dir.exists(path)) {
+    dir.create(path)
+  }
+
+  # create the css file
+  fname <- paste0("palette_", name, ".css")
+  if(file.exists(file.path(path, fname)) & overwrite == FALSE) {
+    stop(name, "palette already exists in static/css/")
+  } else {
+    writeLines(pal, file.path(path, fname))
+  }
+
+}
+
