ff95d4247283d393296412001fc08d567263b08f
Danielle Navarro <d.navarro@unsw.edu.au>
2019-05-10 17:18:42 +1000

    give up on travis tests for remote install

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,6 @@ library(testthat)
 library(slumdown)
 
 # make sure the machine has hugo installed
-blogdown::install_hugo(extended = TRUE)
+# blogdown::install_hugo(extended = TRUE)
 
 test_check("slumdown")
