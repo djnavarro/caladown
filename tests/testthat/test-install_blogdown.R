9e7d816f465b00d383bdca41e0c81a92ea48759b
Danielle Navarro <d.navarro@unsw.edu.au>
2020-07-15 19:36:57 +1000

    adds blogdown hugo install

--- tests/testthat/test-install_blogdown.R
+++ tests/testthat/test-install_blogdown.R
@@ -1,5 +1,8 @@
 test_that("blogdown install works", {
 
+  # install hugo if need be
+  blogdown::install_hugo("0.66.0")
+
   # check that something installs
   path <- tempfile()
   result <- try(create_blogdown_calade(path, serve = FALSE, install_hugo = FALSE))
